ad610b25118a23c72c4210df7c9daaac4ebe56cc
Hien Nguyen <hienng2007@gmail.com>
2026-01-07 21:24:17 -0800

    Added new classes

--- src/main/java/Objects/Rank.java
+++ src/main/java/Objects/Rank.java
@@ -1,5 +1,26 @@
 package Objects;
 
-public interface Rank {
+import java.util.Date;
+
+public class Rank {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    private Date date;
 
 }
