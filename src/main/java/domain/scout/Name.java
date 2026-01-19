edff0184f9bd4413aebf2db46347ce191da7c015
Hien Nguyen <hienng2007@gmail.com>
2026-01-18 22:34:06 -0800

    Add Name class

---
+++ src/main/java/domain/scout/Name.java
@@ -0,0 +1,42 @@
+package domain.scout;
+
+import java.util.Objects;
+
+public class Name {
+    private String firstName;
+    private String lastName;
+    private String middleName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    @Override
+    public String toString() {
+        return getFullName();
+    }
+}
